6699b9e657f6d853aefa9f1ed72fcbbd846b887e
AML <89642087+po79tvx@users.noreply.github.com>
2022-02-11 11:12:25 +0100

    First version of a mysql csharp program to create, delete databases/tables, and so on...

---
+++ Projet_MySQL/frmDatabase.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Projet_MySQL
+{
+    partial class frmDatabase
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblIsConnected = new System.Windows.Forms.Label();
+            this.btnAddDb = new System.Windows.Forms.Button();
+            this.txtNomDb = new System.Windows.Forms.TextBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnDeleteDb = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddRow = new System.Windows.Forms.Button();
+            this.btnDeleteRow = new System.Windows.Forms.Button();
+            this.btnCreateTable = new System.Windows.Forms.Button();
+            this.btnDeleteTable = new System.Windows.Forms.Button();
+            this.btnFinaliser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(167, 40);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connexion";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblIsConnected
+            // 
+            this.lblIsConnected.AutoSize = true;
+            this.lblIsConnected.Location = new System.Drawing.Point(185, 14);
+            this.lblIsConnected.Name = "lblIsConnected";
+            this.lblIsConnected.Size = new System.Drawing.Size(66, 13);
+            this.lblIsConnected.TabIndex = 1;
+            this.lblIsConnected.Text = "Déconnecté";
+            // 
+            // btnAddDb
+            // 
+            this.btnAddDb.Location = new System.Drawing.Point(13, 58);
+            this.btnAddDb.Name = "btnAddDb";
+            this.btnAddDb.Size = new System.Drawing.Size(166, 40);
+            this.btnAddDb.TabIndex = 2;
+            this.btnAddDb.Text = "Ajouter une base de donnée";
+            this.btnAddDb.UseVisualStyleBackColor = true;
+            this.btnAddDb.Click += new System.EventHandler(this.btnAddDb_Click);
+            // 
+            // txtNomDb
+            // 
+            this.txtNomDb.Location = new System.Drawing.Point(550, 32);
+            this.txtNomDb.Name = "txtNomDb";
+            this.txtNomDb.Size = new System.Drawing.Size(283, 20);
+            this.txtNomDb.TabIndex = 3;
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(550, 58);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(643, 371);
+            this.dgvData.TabIndex = 4;
+            this.dgvData.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvData_CellValueChanged);
+            // 
+            // btnDeleteDb
+            // 
+            this.btnDeleteDb.Location = new System.Drawing.Point(13, 102);
+            this.btnDeleteDb.Name = "btnDeleteDb";
+            this.btnDeleteDb.Size = new System.Drawing.Size(166, 40);
+            this.btnDeleteDb.TabIndex = 5;
+            this.btnDeleteDb.Text = "Supprimer une base de donnée";
+            this.btnDeleteDb.UseVisualStyleBackColor = true;
+            this.btnDeleteDb.Click += new System.EventHandler(this.btnDeleteDb_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(547, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nom de la table";
+            // 
+            // btnAddRow
+            // 
+            this.btnAddRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddRow.Location = new System.Drawing.Point(550, 435);
+            this.btnAddRow.Name = "btnAddRow";
+            this.btnAddRow.Size = new System.Drawing.Size(40, 40);
+            this.btnAddRow.TabIndex = 7;
+            this.btnAddRow.Text = "+";
+            this.btnAddRow.UseVisualStyleBackColor = true;
+            this.btnAddRow.Click += new System.EventHandler(this.BtnAddRow_Click);
+            // 
+            // btnDeleteRow
+            // 
+            this.btnDeleteRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteRow.Location = new System.Drawing.Point(596, 435);
+            this.btnDeleteRow.Name = "btnDeleteRow";
+            this.btnDeleteRow.Size = new System.Drawing.Size(40, 40);
+            this.btnDeleteRow.TabIndex = 8;
+            this.btnDeleteRow.Text = "-";
+            this.btnDeleteRow.UseVisualStyleBackColor = true;
+            this.btnDeleteRow.Click += new System.EventHandler(this.BtnDeleteRow_Click);
+            // 
+            // btnCreateTable
+            // 
+            this.btnCreateTable.Location = new System.Drawing.Point(12, 183);
+            this.btnCreateTable.Name = "btnCreateTable";
+            this.btnCreateTable.Size = new System.Drawing.Size(166, 40);
+            this.btnCreateTable.TabIndex = 9;
+            this.btnCreateTable.Text = "Créer une table";
+            this.btnCreateTable.UseVisualStyleBackColor = true;
+            this.btnCreateTable.Click += new System.EventHandler(this.BtnCreateTable_Click);
+            // 
+            // btnDeleteTable
+            // 
+            this.btnDeleteTable.Location = new System.Drawing.Point(12, 227);
+            this.btnDeleteTable.Name = "btnDeleteTable";
+            this.btnDeleteTable.Size = new System.Drawing.Size(166, 40);
+            this.btnDeleteTable.TabIndex = 10;
+            this.btnDeleteTable.Text = "Supprimer une table";
+            this.btnDeleteTable.UseVisualStyleBackColor = true;
+            // 
+            // btnFinaliser
+            // 
+            this.btnFinaliser.Location = new System.Drawing.Point(642, 435);
+            this.btnFinaliser.Name = "btnFinaliser";
+            this.btnFinaliser.Size = new System.Drawing.Size(166, 40);
+            this.btnFinaliser.TabIndex = 11;
+            this.btnFinaliser.Text = "Finaliser la table";
+            this.btnFinaliser.UseVisualStyleBackColor = true;
+            this.btnFinaliser.Click += new System.EventHandler(this.BtnFinaliser_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 649);
+            this.Controls.Add(this.btnFinaliser);
+            this.Controls.Add(this.btnDeleteTable);
+            this.Controls.Add(this.btnCreateTable);
+            this.Controls.Add(this.btnDeleteRow);
+            this.Controls.Add(this.btnAddRow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDeleteDb);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.txtNomDb);
+            this.Controls.Add(this.btnAddDb);
+            this.Controls.Add(this.lblIsConnected);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblIsConnected;
+        private System.Windows.Forms.Button btnAddDb;
+        private System.Windows.Forms.TextBox txtNomDb;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnDeleteDb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddRow;
+        private System.Windows.Forms.Button btnDeleteRow;
+        private System.Windows.Forms.Button btnCreateTable;
+        private System.Windows.Forms.Button btnDeleteTable;
+        private System.Windows.Forms.Button btnFinaliser;
+    }
+}
+
---
+++ Projet_MySQL/frmDatabase.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Projet_MySQL
+{
+    public partial class frmDatabase : Form
+    {
+        string MyConnectionString = "Server=localhost;Uid=root;Pwd=root;Database=mysql;Charset=latin1";
+        public bool isConnected = false;
+        MySqlConnection myConnection;
+        string myTable;
+
+        public frmDatabase()
+        {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Connect the user to the mysql database
+        /// <!--TODO-->
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            myConnection = new MySqlConnection(MyConnectionString);
+
+            if (isConnected == true)
+            {
+                myConnection.Close();
+                btnConnect.Text = "Connexion";
+                lblIsConnected.Text = "Déconnecté";
+                Console.WriteLine("Déconnecté");
+                isConnected = false;
+            }
+            else if (isConnected == false)
+            {
+                myConnection.Open();
+                btnConnect.Text = "Déconnexion";
+                lblIsConnected.Text = $"Connecté à {myConnection.Database}";
+                Console.WriteLine("Connecté");
+                isConnected = true;
+            }
+        }
+
+        /// <summary>
+        /// Create a new database
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void btnAddDb_Click(object sender, EventArgs e)
+        {
+            if (isConnected == true)
+            {
+                //Try if the name of the database is not empty
+                if (txtNomDb.Text != string.Empty)
+                {
+                    //Try if the database exist
+                    try
+                    {
+                            MySqlCommand myCmd = myConnection.CreateCommand();
+                            myCmd.CommandText = $"CREATE DATABASE if not exists {txtNomDb.Text}";
+                            MySqlDataAdapter myAdaptater = new MySqlDataAdapter(myCmd);
+                            DataSet myDataSet = new DataSet();
+                            myAdaptater.Fill(myDataSet);
+                            //dgvData.DataSource = myDataSet.Tables[0].DefaultView;
+                    }
+                    catch (Exception) 
+                     {
+                            MessageBox.Show("La base de donnée que vous essaiez de créer existe déjà !", "My_SQL Error");
+                    }; 
+                }
+                else
+                {
+                    ShowEmpty();
+                }
+            }
+            else
+            {
+                ShowConnected();
+            }
+        }
+
+        /// <summary>
+        /// Delete an entire database
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void btnDeleteDb_Click(object sender, EventArgs e)
+        {
+            if (isConnected == true )
+            {
+                if (txtNomDb.Text != string.Empty)
+                {
+                        try
+                        {
+                            MySqlCommand myCmd = myConnection.CreateCommand();
+                            myCmd.CommandText = $"drop database if exists {txtNomDb.Text}";
+                            MySqlDataAdapter myAdaptater = new MySqlDataAdapter(myCmd);
+                            DataSet myDataSet = new DataSet();
+                            myAdaptater.Fill(myDataSet);
+                        }
+                        catch (Exception) 
+                        {
+                            MessageBox.Show("La base de donnée que vous essaiez de supprimer n'existe pas !", "My_SQL Error");
+                        };
+                }
+                else
+                {
+                    ShowEmpty();
+                }
+            }
+            else
+            {
+                ShowConnected();
+            }
+        }
+
+        /// <summary>
+        /// Show if a field is empty
+        /// Temporary Method
+        /// </summary>
+        private void ShowEmpty()
+        {
+            MessageBox.Show("Veuillez remplir le champ du nom de la base de donnée !", "My_SQL Error");
+        }
+
+        /// <summary>
+        /// Show if the user is not connected
+        /// Temporary Method
+        /// </summary>
+        private void ShowConnected()
+        {
+            MessageBox.Show("Veuillez vous connecter au SGBD !", "My_SQL Error");
+        }
+
+        /// <summary>
+        /// Add a row in the data view
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void BtnAddRow_Click(object sender, EventArgs e)
+        {
+            dgvData.Rows.Add(1);
+        }
+
+        /// <summary>
+        /// Delete a row in the data view
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void BtnDeleteRow_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in dgvData.SelectedRows)
+            {
+                dgvData.Rows.RemoveAt(item.Index);
+            }
+        }
+
+        /// <summary>
+        /// Create a new data view
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void BtnCreateTable_Click(object sender, EventArgs e)
+        {
+            //Clear
+            dgvData.Columns.Clear();
+            dgvData.Refresh();
+
+            //Create all the columns with name and inputs in them
+            if (isConnected == true)
+            {
+                DataGridViewColumn dc_Nom = new DataGridViewTextBoxColumn();
+                DataGridViewColumn dc_Type = new DataGridViewComboBoxColumn();
+                DataGridViewColumn dc_TailleVal = new DataGridViewTextBoxColumn();
+                DataGridViewColumn dc_IsNull = new DataGridViewCheckBoxColumn();
+                DataGridViewColumn dc_IsPrimaryKey = new DataGridViewCheckBoxColumn();
+                DataGridViewColumn dc_Comment = new DataGridViewTextBoxColumn();
+
+                dc_Nom.HeaderText = "Nom";
+                dc_Type.HeaderText = "Type";
+                dc_TailleVal.HeaderText = "Taille/Valeurs";
+                dc_IsNull.HeaderText = "Null ?";
+                dc_IsPrimaryKey.HeaderText = "PrimaryKey ?";
+                dc_Comment.HeaderText = "Commentaire";
+
+
+                DataGridViewComboBoxCell myComboCell = new DataGridViewComboBoxCell();
+                myComboCell.Items.AddRange("INT", "CHAR", "STRING", "DECIMAL", "DATE", "VARCHAR", "BOOLEAN", "FLOAT", "DATETIME");
+                dc_Type.CellTemplate = myComboCell;
+
+                dgvData.Columns.AddRange(dc_Nom, dc_Type, dc_TailleVal, dc_IsNull, dc_IsPrimaryKey, dc_Comment);
+            }
+            else
+            {
+                ShowConnected();
+            }
+            
+        }
+
+        /// <summary>
+        /// <<!--TODO-->
+        /// Finalise the creation of the table
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void BtnFinaliser_Click(object sender, EventArgs e)
+        {
+            //Make the table string empty
+            myTable = "";
+
+            //Try if the user is connected to a database
+            if (isConnected == true)
+            {
+                /*MySqlCommand myCmd = myConnection.CreateCommand();
+                myCmd.CommandText = $"drop database if exists {txtNomDb.Text}";
+                myCmd.CommandText = $"CREATE TABLE IF NOT EXISTS {txtNomDb.Text}";*/
+
+                //Start the creation of the table request
+                myTable += $"CREATE TABLE IF NOT EXISTS {txtNomDb.Text}(";
+
+                //Reach the rows of the data view
+                foreach (DataGridViewRow row in dgvData.Rows)
+                {
+                    //Reach the cells in the rows
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        try
+                        {
+                            Type cellType = cell.Value.GetType();
+
+                            //If a "ID" checkbox is checked
+                            if (cellType.Equals(typeof(bool)) && cell.ColumnIndex == 4 && (bool)cell.Value == true)
+                            {
+                                myTable += $"AUTO_INCREMENT";
+                            }
+                            //IF a null checkbox is checked
+                            else if (cellType.Equals(typeof(bool)) && cell.ColumnIndex == 3 && (bool)cell.Value == true)
+                            {
+                                myTable += $"NOT NULL";
+                            }
+                            //The others
+                            else
+                            {
+                                myTable += $"{cell.Value} ";
+                            }
+                        }
+                        catch (Exception) { };
+                                            }
+                    myTable += "\n";
+                }
+            }
+
+            //Show the result of the request in sql "style"
+            MessageBox.Show(myTable);
+        }
+
+        /// <summary>
+        /// Let the primary checkbox being checked only one time in a column
+        /// Taked on StackOverflow : https://stackoverflow.com/questions/35350584/allow-only-one-checkbox-selected-in-datagridview
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">E</param>
+        private void DgvData_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            var columnIndex = 4;
+            if (e.ColumnIndex == columnIndex)
+            {
+                var isChecked = (bool)dgvData.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+                if (isChecked)
+                {
+                    foreach (DataGridViewRow row in dgvData.Rows)
+                    {
+                        if (row.Index != e.RowIndex)
+                        {
+                            row.Cells[columnIndex].Value = !isChecked;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
